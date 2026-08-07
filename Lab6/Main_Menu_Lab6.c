4d20fbcf4ea84bcc8a0383a0c1f59361e785d1d4
danhpham74 <danhpham742007@gmail.com>
2026-08-07 20:49:01 +0700

    hoàn thành lab6

--- Lab6/Main_Menu_Lab6.c
+++ Lab6/Main_Menu_Lab6.c
@@ -1,24 +1,142 @@
 #include <stdio.h>
+#include <stdbool.h>
+#include <math.h>
+#include <float.h>
+void menu();
+void chucNang1();
+void chucNang2();
+void chucNang3();
+void chucNang4();
+void chucNang5();
+
+float tinhTrungBinhCong(int a[], int n);
+void timMaxMin(int a[], int n);
+void sapXepGiamDan(int a[], int n);
+void maTranBinhPhuong(int row, int col);
+void xuatSoLeMaTran(int row, int col);
+void swap(int *a, int *b);
+int main()
+{
+    int chon;
+    do
+    {
+        menu();
+        printf("Ban hay chon chuc nang tu (1-6): ");
+        scanf("%d", &chon);
+        switch (chon)
+        {
+        case 1:
+            chucNang1();
+            break;
+        case 2:
+            chucNang2();
+            break;
+        case 3:
+            chucNang3();
+            break;
+        case 4:
+            chucNang4();
+            break;
+        case 5:
+            chucNang5();
+            break;
+        case 6:
+            printf("Ban da thoat chuong trinh");
+            break;
+        default:
+            printf("Ban phai chon chuc nang tu 1 - 6\n");
+            break;
+        }
+    } while (chon != 6);
+
+    return 0;
+}
 
 void menu()
 {
-    printf("+-------------------------------------------------------------+\n");
-    printf("|              MENU CHUONG TRINH LAB 6                        |\n");
-    printf("+.............................................................+\n");
-    printf("|1.Chuc nang tinh trung binh tong cac so chia het cho 3 va 5  |\n");
-    printf("|2.Chuc nang tim gia tri lon nhat va nho nhat trong mang      |\n");
-    printf("|3.Chuc nang sap xep mang theo thu tu giam dan                |\n");
-    printf("|4.Chuc nang ma tran binh phuong(mang 2 chieu)                |\n");
-    printf("|5.Chuc nang loc va xuat cac so le trong ma tran              |\n");
-    printf("|6.Thoat chuong trinh                                         |\n");
-    printf("+-------------------------------------------------------------+\n");
-    printf("Ban hay chon chuc nang 1-6: ");
+    printf("+---------------------------------------------------+\n");
+    printf("|             MENU CHUONG TRINH LAB 6               |\n");
+    printf("+---------------------------------------------------+\n");
+    printf("| 1. Tinh trung binh tong so chia het cho 3 va 5    |\n");
+    printf("| 2. Tim gia tri lon nhat va nho nhat trong mang    |\n");
+    printf("| 3. Sap xep mang theo gia tri giam dan             |\n");
+    printf("| 4. Ma tran binh phuong(mang 2 chieu)              |\n");
+    printf("| 5. Loc va xuat ca vi tri so le trong ma tran      |\n");
+    printf("| 6. Thoat chuong trinh                             |\n");
+    printf("+---------------------------------------------------+\n");
 }
-void tinhTrungBinhCong(int a[], int n)
+
+void chucNang1()
+{
+    int n;
+    int a[10];
+    printf("Nhap n: ");
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++)
+    {
+        printf("Nhap A[%d]= ", i);
+        scanf("%d", &a[i]);
+    }
+    float tbc = tinhTrungBinhCong(a, n);
+    if (tbc == -FLT_MAX)
+    {
+        printf("Khong co so chia het cho 3 va 5\n");
+    }
+    else
+    {
+        printf("Trung binh cong cua mang: %.2f\n", tbc);
+    }
+}
+
+void chucNang2()
+{
+    int n;
+    int a[10];
+    printf("Nhap n: ");
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++)
+    {
+        printf("Nhap A[%d]= ", i);
+        scanf("%d", &a[i]);
+    }
+    timMaxMin(a, n);
+}
+
+void chucNang3()
+{
+    int n;
+    int a[10];
+    printf("Nhap n: ");
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++)
+    {
+        printf("Nhap A[%d]= ", i);
+        scanf("%d", &a[i]);
+    }
+    sapXepGiamDan(a, n);
+}
+
+void swap(int *a, int *b)
+{
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+void chucNang4()
+{
+    maTranBinhPhuong(3, 3);
+}
+
+void chucNang5()
+{
+    xuatSoLeMaTran(3, 3);
+}
+
+float tinhTrungBinhCong(int a[], int n)
 {
     int tong = 0;
     int soDem = 0;
-
     for (int i = 0; i < n; i++)
     {
         if (a[i] % 3 == 0 && a[i] % 5 == 0)
@@ -27,64 +145,110 @@ void tinhTrungBinhCong(int a[], int n)
             soDem++;
         }
     }
-
     if (soDem == 0)
     {
-        printf("\n=> Khong co so chia het cho 3 va 5\n\n");
+        return -FLT_MAX;
     }
     else
     {
-        printf("\n=> Trung binh tong cac so chia het cho 3 va 5 la : %.2f\n\n", (float)tong / soDem);
+        return (float)tong / soDem;
     }
 }
-int main()
+
+void timMaxMin(int a[], int n)
 {
-    int chon;
 
-    do
+    int min, max;
+    min = a[0];
+    max = a[0];
+    for (int i = 1; i < n; i++)
     {
-        menu();
-        scanf("%d", &chon);
-
-        switch (chon)
+        if (min > a[i])
         {
-        case 1:
-        
-            int n;
-            int a[100];
 
-            printf("Nhap n: ");
-            scanf("%d", &n);
-
-            for (int i = 0; i < n; i++)
-            {
-                printf("Nhap A[%d] = ", i);
-                scanf("%d", &a[i]);
-                tinhTrungBinhCong(a, n);
-                break;
-            }
-        case 2:
-            printf("\nChuc nang 2\n\n");
-            break;
-        case 3:
-            printf("\nChuc nang 3\n\n");
-            break;
-        case 4:
-            printf("\nChuc nang 4\n\n");
-            break;
-        case 5:
-            printf("\nChuc nang 5\n\n");
-            break;
-        case 6:
-            printf("\nBan da chon thoat chuong trinh!\n");
-            break;
-        default:
-            printf("\nVui long chon tu 1-6!\n\n");
-            break;
+            min = a[i];
         }
-        }
-        while (chon != 6)
-            ;
+        if (max < a[i])
+        {
 
-        return 0;
+            max = a[i];
+        }
     }
+    printf("Min = %d\t Max = %d\n", min, max);
+}
+
+void sapXepGiamDan(int a[], int n)
+{
+
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = i + 1; j < n; j++)
+        {
+            if (a[i] > a[j])
+            {
+                swap(&a[i], &a[j]);
+            }
+        }
+    }
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d\t", a[i]);
+    }
+    printf("\n");
+}
+
+void maTranBinhPhuong(int row, int col)
+{
+    int a[row][col];
+
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
+            printf("A[%d,%d] = ", i, j);
+            scanf("%d", &a[i][j]);
+        }
+    }
+
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
+            printf("%4d\t", a[i][j] * a[i][j]);
+        }
+        printf("\n");
+    }
+}
+void xuatSoLeMaTran(int row, int col)
+{
+    int a[row][col];
+
+    printf("Nhap ma tran (%dx%d):\n", row, col);
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
+            printf("A[%d][%d] = ", i, j);
+            scanf("%d", &a[i][j]);
+        }
+    }
+
+    printf("\nCac phan tu le trong ma tran:\n");
+    int soDem = 0;
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
+            if (a[i][j] % 2 != 0)
+            {
+                printf("Phan tu le tai a[%d][%d] = %d\n", i, j, a[i][j]);
+                soDem++;
+            }
+        }
+    }
+
+    if (soDem == 0)
+    {
+        printf("Khong co so le nao trong ma tran.\n");
+    }
+}
